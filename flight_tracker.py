1f7e918cdc5c83c161ca01d6a796abcdb5abbb26
darkgullgray <ettciu@gmail.com>
2026-05-28 18:09:02 +0200

    Update flight_tracker.py

--- flight_tracker.py
+++ flight_tracker.py
@@ -1,1 +1,51 @@
+from playwright.sync_api import sync_playwright
+import smtplib
+from email.mime.text import MIMEText
+from datetime import datetime
 
+FROM_EMAIL = "TUA_MAIL@gmail.com"
+APP_PASSWORD = "PASSWORD_APP_GMAIL"
+TO_EMAIL = "TUA_MAIL@gmail.com"
+
+URL = "https://www.google.com/travel/flights"
+
+
+def get_flight_price():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+
+        page.goto(URL)
+
+        page.wait_for_timeout(10000)
+
+        content = page.content()
+
+        browser.close()
+
+        return "Prezzo trovato manualmente"
+
+
+price = get_flight_price()
+
+message = f"""
+Monitor voli Giappone
+
+Data: {datetime.now()}
+
+Risultato:
+{price}
+"""
+
+msg = MIMEText(message)
+msg["Subject"] = "Report voli Giappone"
+msg["From"] = FROM_EMAIL
+msg["To"] = TO_EMAIL
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login(FROM_EMAIL, APP_PASSWORD)
+server.send_message(msg)
+server.quit()
+
+print("Email inviata")
