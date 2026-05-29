b793eeb9651d37789784102498f20fb7ea7b5efc
darkgullgray <ettciu@gmail.com>
2026-05-29 16:49:32 +0200

    Update flight_tracker.py

--- flight_tracker.py
+++ flight_tracker.py
@@ -14,29 +14,29 @@ URL = "https://www.google.com/travel/flights"
 def get_flight_price():
     test_url = "https://www.google.com/travel/flights/search?tfs=CBwQAhoeEgoyMDI2LTExLTAyagcIARIDRkNPcgcIARIDS0lYGh4SCjIwMjYtMTEtMTZqBwgBEgNLSVhyBwgBEgNGQ09AAUABQAFIAXABggELCP___________wGYAQE&hl=it&gl=IT"
 
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        page = browser.new_page()
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+    page = browser.new_page()
 
-        page.goto(test_url, wait_until="networkidle")
-        page.wait_for_timeout(10000)
+    page.goto(test_url, wait_until="networkidle")
+    page.wait_for_timeout(10000)
 
-        testo = page.locator("body").inner_text()
+    testo = page.locator("body").inner_text()
 
-        browser.close()
+    browser.close()
 
-        prezzo = "Non trovato"
-        valutazione = "Non trovata"
+prezzo = "Non trovato"
+valutazione = "Non trovata"
 
-        righe = testo.split("\n")
+righe = testo.split("\n")
 
-    for i, riga in enumerate(righe):
-     if "da " in riga and "€" in riga:
+for i, riga in enumerate(righe):
+    if "da " in riga and "€" in riga:
         prezzo = riga.strip()
         break
 
-    for riga in righe:
-     if "Al momento, i prezzi" in riga:
+for riga in righe:
+    if "Al momento, i prezzi" in riga:
         valutazione = riga.strip()
         break
 
@@ -49,7 +49,10 @@ Prezzo minimo:
 
 Valutazione Google:
 {valutazione}
-      return report
+
+
+        return report
+
 
 
 
@@ -57,9 +60,10 @@ price = get_flight_price()
 
 message = f"""
 
+
 Data: {datetime.now()}
 
-Risultato:
+
 {price}
 
 
